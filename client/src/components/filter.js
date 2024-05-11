fed5e32c5db6a13c7bbec54215806950c050eeef
JCGM1047 <jcgm1047@gmail.com>
2024-05-10 19:29:31 -0500

    10/05/24

--- client/src/components/filter.js
+++ client/src/components/filter.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "./styles/filters.module.scss"; // Importa los estilos
 
 function Filters({ onFilterChange }) {
   const [genre, setGenre] = useState("");
@@ -22,39 +23,38 @@ function Filters({ onFilterChange }) {
   return (
     <form
       onSubmit={handleSubmit}
-      className="row g-3 align-items-center justify-content-center"
+      className={`${styles.formWrapper} row g-3 align-items-center justify-content-center`}
     >
       <div className="col-auto">
         <select
           value={genre}
           onChange={(e) => setGenre(e.target.value)}
-          className="form-select"
+          className={`${styles.formSelect} form-select`}
           aria-label="Default select example"
         >
           <option value="">Seleccionar el género</option>
-          <option value="28">Accion</option>
+          <option value="28">Acción</option>
           <option value="35">Comedia</option>
           <option value="18">Drama</option>
           <option value="27">Horror</option>
           <option value="878">Ciencia Ficción</option>
-          <option value="14">Fantasia</option>
+          <option value="14">Fantasía</option>
           <option value="53">Thriller</option>
           <option value="10749">Romance</option>
           <option value="99">Documentales</option>
           <option value="12">Aventuras</option>
           <option value="10402">Musical</option>
           <option value="16">Animación</option>
-          <option value="9648">Mysterio</option>
-          <option value="10752">Gerra</option>
-          <option value="37">Viejo Oest</option>
-          {/* Lista de géneros */}
+          <option value="9648">Misterio</option>
+          <option value="10752">Guerra</option>
+          <option value="37">Western</option>
         </select>
       </div>
       <div className="col-auto">
         <select
           value={year}
           onChange={(e) => setYear(e.target.value)}
-          className="form-select"
+          className={`${styles.formSelect} form-select`}
         >
           <option value="">Seleccionar el año</option>
           {years.map((y) => (
